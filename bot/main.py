33e44775493a2fd585ab26370e83865346c4bb37
Replit user <replituser@example.com>
2021-03-18 13:22:52 +0000

    up-to-date

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,11 @@
 import discord
 import os
-#import pynacl
-#import dnspython
+import pynacl
+import dnspython
 import server
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="+")
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
