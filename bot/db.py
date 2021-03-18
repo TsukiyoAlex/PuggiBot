ea57bab5867dc13fff3fdfa19eafa4f2b8db2182
Replit user <replituser@example.com>
2021-03-18 14:03:47 +0000

    up-to-date

---
+++ bot/db.py
@@ -0,0 +1,9 @@
+x0='https://pbs.twimg.com/media/Ewm6ZVuWYAs4ri4?format=jpg&name=large'
+x1='https://pbs.twimg.com/media/Ewm6YMAXAAI9IU3?format=jpg&name=large'
+x2='https://pbs.twimg.com/media/Ewm6aiqWgAQk19v?format=jpg&name=large'
+x3='https://pbs.twimg.com/media/EwH9Cs1XYAQY8Pq?format=jpg&name=large'
+x4='https://pbs.twimg.com/media/EwH9BTWXcAQFjUV?format=jpg&name=large'
+
+p0 = [x4]
+p1 = [x2]
+p2 = [x0,x1,x3]
