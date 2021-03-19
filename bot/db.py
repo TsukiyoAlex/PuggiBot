9103b855ff9cfc6adacbf0d80bd2d35841566548
TsukiyoAlex <alexandros.aug@gmail.com>
2021-03-19 00:44:29 +0000

    up-to-date

--- bot/db.py
+++ bot/db.py
@@ -3,8 +3,9 @@ x1='https://i.postimg.cc/xjgr49wm/Ewm6aiq-Wg-AQk19v.jpg'
 x2='https://i.postimg.cc/PJ57j8gw/Ew-H9-Cs1-XYAQY8-Pq.jpg'
 x3='https://i.postimg.cc/sD8NkxwM/Ewm6-YMAXAAI9-IU3.jpg'
 x4='https://i.postimg.cc/283t9hFp/Ewm6-ZVu-WYAs4ri4.jpg'
+x5='https://i.postimg.cc/RVr8QHrC/tenor.gif'
 
-p0 = [x0]
+p0 = [x0,x5]
 p1 = [x1]
 p2 = [x2]
 p3 = [x3,x4]
