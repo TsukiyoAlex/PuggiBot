17cb8e0f9b17fcd6c65f8c16240198318c33f83e
TsukiyoAlex <alexandros.aug@gmail.com>
2021-03-31 23:50:10 +0000

    v1.1.4.1

---
+++ bot/aprilfools.py
@@ -0,0 +1,15 @@
+puggi = [
+'https://i.postimg.cc/B6pQBcXx/b0037s3-figure.png',
+'https://i.postimg.cc/SRtSwSLR/b0037s4-figure.png',
+'https://i.postimg.cc/R09BpJrG/b0037s5-a2-figure.png',
+'https://i.postimg.cc/ZqchGY4m/b0037s5-figure.png',
+'https://i.postimg.cc/W10V76yh/b0202s5-figure.png'
+]
+
+pang = [
+'https://i.postimg.cc/PJwLzV8P/1562858048-wolfmalro-20190630.jpg',
+'https://i.postimg.cc/tTfZ6GJK/1604837783-zoosp-pang-sp.jpg',
+'https://i.postimg.cc/qBz5nPjj/aesv6w1nyak21.png',
+'https://i.postimg.cc/bdfZ60QL/dfzzahvotnx41.jpg',
+'https://i.postimg.cc/sX1G1gjP/Dp-Xd-INr-U8-AAKp5-H.jpg'
+]
--- bot/main.py
+++ bot/main.py
@@ -10,15 +10,21 @@ from discord.ext import commands
 from random import choices
 from discord.ext.commands import cooldown, BucketType
 from pretty_help import PrettyHelp
-from puggibotdb import *
+#from puggibotdb import *
+from aprilfools import *
 
 bot = commands.Bot(command_prefix="+",
-description = 'PuggiBot v1.0',
+description = 'PuggiBot v1.1.4.1',
 help_command = PrettyHelp(no_category='Commands',show_index = False))
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-r = [p0, p1, p2, p3]
-w = [50, 25.5, 18.5, 6]
+#r = [p0, p1, p2, p3]
+#w = [50, 25.5, 18.5, 6]
+
+r = [puggi, pang]
+miracle = 1/2578917
+common = 1 - miracle
+w = [common, miracle]
 
 @bot.event
 async def on_ready():
@@ -34,11 +40,18 @@ async def ping(ctx):
     await ctx.send(s)
 
 @bot.command(name='pull', help='Pulls a random image. Only 10 pulls per hour allowed for each user!')
-@commands.cooldown(10, 3600, commands.BucketType.user)
+@commands.cooldown(10, 180, commands.BucketType.user)
 async def pull(ctx):
+    #x = random.choices(r,w)
+    #y = random.choice(x[0])
+    #await ctx.send(y)
     x = random.choices(r,w)
     y = random.choice(x[0])
-    await ctx.send(y)
+    if (x[0] == pang):
+      await ctx.send('<@303857311233867776>')
+      await ctx.send(y)
+    else:
+      await ctx.send(y)
 
 @pull.error
 async def pull_error(ctx, error):
@@ -56,7 +69,10 @@ async def pull_error(ctx, error):
 
 @bot.command(name='rates', help='Check the rates for each rarity!')
 async def rates(ctx):
-    await ctx.send('<:star_N:822188326114557984> = '+str(w[0])+'%\n<:star_R:822188398965293186> = '+str(w[1])+'%\n<:star_SR:822188419853058070> = '+str(w[2])+'%\n<:star_SSR:822188441361055774> = '+str(w[3])+'%')
+    #await ctx.send('<:star_N:822188326114557984> = '+str(w[0])+'%\n<:star_R:822188398965293186> = '+str(w[1])+'%\n<:star_SR:822188419853058070> = '+str(w[2])+'%\n<:star_SSR:822188441361055774> = '+str(w[3])+'%')
+    common_rate = common*100
+    miracle_rate = miracle*100
+    await ctx.send('<:puggihandsome:808627398794018827> = '+str(common_rate)+'%\n<:pangblush:686774020443602944> = '+str(miracle_rate)+'%')
 
 server.server()
 bot.run(TOKEN)
