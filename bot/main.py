0904b82b07b0b8e49f4e4b20304ecc62ed38b205
TsukiyoAlex <>
2021-04-17 03:44:30 +0000

    added hello

--- bot/main.py
+++ bot/main.py
@@ -13,61 +13,86 @@ from pretty_help import PrettyHelp
 from puggibotdb import *
 
 bot = commands.Bot(command_prefix="+",
-description = 'PuggiBot v1.2.5',
-help_command = PrettyHelp(no_category='Commands',show_index = False))
+                   description='PuggiBot v1.2.5',
+                   help_command=PrettyHelp(no_category='Commands',
+                                           show_index=False))
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 counting = 0
 r = [p0, p1, p2, p3]
 w = [50, 25.5, 18.5, 6]
 
+
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you suffer"))
+    await bot.change_presence(activity=discord.Activity(
+        type=discord.ActivityType.watching, name="you suffer"))
     print(f"Logged in as {bot.user.name}({bot.user.id})")
+
 
 @bot.command(name='ping', help='IT WORKS!')
 async def ping(ctx):
-    e = ["**Pang!** <:pangblush:649970659358933002>",
-    "**Pang!** <:wangannoyed:792849470877859876>",
-    "**Pang!** <:spangwhat:792849432038473768>"]
+    e = [
+        "**Pang!** <:pangblush:649970659358933002>",
+        "**Pang!** <:wangannoyed:792849470877859876>",
+        "**Pang!** <:spangwhat:792849432038473768>"
+    ]
     s = random.choice(e)
     await ctx.send(s)
 
-@bot.command(name='pull', help='Pulls a random image. Max 10 images per hour for each user!')
+@bot.command(pass_context=True)
+async def hello(ctx):
+    member = ctx.author
+    role = discord.utils.get(member.guild.roles, name="Fredrica")
+    if(member.id == 303857311233867776):
+    await member.add_roles(role)
+    else:
+      await ctx.send("Invalid!")
+
+@bot.command(name='pull',
+             help='Pulls a random image. Max 10 images per hour for each user!'
+             )
 @commands.cooldown(10, 3600, commands.BucketType.user)
 async def pull(ctx):
     global counting
     counting = counting
     if (counting < 9):
-      x = random.choices(r,w)
-      y = random.choice(x[0])
-      counting = counting + 1
+        x = random.choices(r, w)
+        y = random.choice(x[0])
+        counting = counting + 1
     else:
-      y = random.choice(v)
-      counting = 0
+        y = random.choice(v)
+        counting = 0
 
     await ctx.send(y)
+
 
 @pull.error
 async def pull_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
         sec = error.retry_after
-        if(sec < 60):
-          msg = '<:puggiFIRE:808672592550297631> PUGGI IS ON FIRE! Cooldown in **{:.0f}** seconds!'.format(sec)
+        if (sec < 60):
+            msg = '<:puggiFIRE:808672592550297631> PUGGI IS ON FIRE! Cooldown in **{:.0f}** seconds!'.format(
+                sec)
         else:
-          minut = sec // 60
-          sec2 = sec % 60
-          msg = '<:puggiFIRE:808672592550297631> PUGGI IS ON FIRE! Cooldown in **{:.0f}** minutes and **{:.0f}** seconds!'.format(minut,sec2)
+            minut = sec // 60
+            sec2 = sec % 60
+            msg = '<:puggiFIRE:808672592550297631> PUGGI IS ON FIRE! Cooldown in **{:.0f}** minutes and **{:.0f}** seconds!'.format(
+                minut, sec2)
 
         y = random.choice
         await ctx.send(msg)
     else:
         raise error
 
+
 @bot.command(name='rates', help='Check the rates for each rarity!')
 async def rates(ctx):
-    await ctx.send('<:star_N:822188326114557984> = '+str(w[0])+'%\n<:star_R:822188398965293186> = '+str(w[1])+'%\n<:star_SR:822188419853058070> = '+str(w[2])+'%\n<:star_SSR:822188441361055774> = '+str(w[3])+'%')
+    await ctx.send('<:star_N:822188326114557984> = ' + str(w[0]) +
+                   '%\n<:star_R:822188398965293186> = ' + str(w[1]) +
+                   '%\n<:star_SR:822188419853058070> = ' + str(w[2]) +
+                   '%\n<:star_SSR:822188441361055774> = ' + str(w[3]) + '%')
+
 
 server.server()
 bot.run(TOKEN)
